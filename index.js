1807a6acea87da1734d62ebe1f872ecc5e1372dc
asdi80 <a8sadi@gmail.com>
2022-01-13 17:04:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function imagersizer(){
+    let images = document.querySelectorAll('.imageresizer');
+    images.forEach(image=>{
+        image.style={
+            width: "1080px",
+            height: "1080px"
+        }
+    })
+
+
+}
+module.exports.imagersizer= imagersizer;
